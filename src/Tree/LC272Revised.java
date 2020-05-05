02ad90c515e8140b53d7139f428f97f0ac29f473
Peng <hao.pe@husky.neu.edu>
2020-05-05 15:34:28 -0700

    LC272 cleaner solution and idea updated

--- src/Tree/LC272Revised.java
+++ src/Tree/LC272Revised.java
@@ -154,3 +154,84 @@ left stack(smaller than target)         right stack (larger than target)
                /                             \
               1                               5
 */
+
+/**
+ * 1. since we need k closet values, so we need some data structure to store and update these k values
+ * 2. we could use a maxHeap(sort by difference) update during traverse o(n) * log(k)
+ * 3. if it is an array:  1 2 3 4 5
+ *                              t
+ *             log(n) + k
+ * 4. so for the tree, we could divide the tree by the closet target ->
+ * smaller and greater ->get closet each time
+ * 5/ to do that, I need 2 stacks to store the left and right part of the trees
+ *
+ * Input: root = [4,2,5,1,3], target = 2.414286, and k = 2
+ *
+ *                2                  3
+ *     4         /                     \
+ *    / \                              4
+ *   2   5
+ *   \
+ *  / \
+ * 1   3         left(smaller)           right
+ *   2.5 3.5
+ */
+class solutionCleaner272{
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null || k == 0) return res;
+        Stack<TreeNode> leftStack = new Stack<>();
+        Stack<TreeNode> rightStack = new Stack<>();
+        //divide to 2 parts
+        while(root != null){
+            if(root.val <= target){
+                //push to left stack
+                leftStack.push(root);
+                root = root.right;
+            }else{
+                rightStack.push(root);
+                root = root.left;
+            }
+        }
+        //add k res
+        while(!leftStack.isEmpty() && !rightStack.isEmpty() && res.size() < k){
+            int leftPeek = leftStack.peek().val, rightPeek = rightStack.peek().val;
+            if(Math.abs(leftPeek - target) < Math.abs(rightPeek - target)){
+                //pop left
+                res.add(leftPeek);
+                //todo:
+                popLeft(leftStack);
+            }else{
+                res.add(rightPeek);
+                //todo:
+                popRight(rightStack);
+            }
+        }
+        while(res.size() < k){
+            if(!leftStack.isEmpty()){
+                res.add(leftStack.peek().val);
+                popLeft(leftStack);
+            }else{
+                res.add(rightStack.peek().val);
+                popRight(rightStack);
+            }
+        }
+        return res;
+    }
+    private void popLeft(Stack<TreeNode> leftStack){
+        TreeNode cur = leftStack.pop();
+        cur = cur.left;
+        while(cur != null){
+            leftStack.push(cur);
+            cur = cur.right;
+        }
+    }
+    private void popRight(Stack<TreeNode> rightStack){
+        TreeNode cur = rightStack.pop();
+        cur = cur.right;
+        while(cur != null){
+            rightStack.push(cur);
+            cur = cur.left;
+        }
+    }
+}
