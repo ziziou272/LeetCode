74fd7683f855a89d64a7755050e045cd5d061af4
Peng <hao.pe@husky.neu.edu>
2020-05-06 14:13:40 -0700

    LC47 permutation updated faster solution

--- src/DFS/LC47.java
+++ src/DFS/LC47.java
@@ -1,10 +1,11 @@
 package DFS;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-public class LC47swap {
+public class LC47 {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if(nums == null || nums.length == 0) return res;
@@ -36,5 +37,31 @@ public class LC47swap {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j]= temp;
+    }
+}
+class solutionCleanFast{
+    // 2^n(length of array), 2^n
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res= new ArrayList<>();
+        dfs(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, List<Integer> path, int[] nums, boolean[] used){
+        if(path.size() == nums.length){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            //deal duplicate
+            if(used[i] || (i - 1 >= 0 && nums[i] == nums[i - 1] && !used[i - 1])){
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            dfs(res, path, nums, used);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
     }
 }
