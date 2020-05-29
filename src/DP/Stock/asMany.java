1286747a2b01162f324642a036e37248a53fbd92
Peng <hao.pe@husky.neu.edu>
2020-05-29 13:30:11 -0700

    stock updated finally

--- src/DP/Stock/asMany.java
+++ src/DP/Stock/asMany.java
@@ -17,3 +17,40 @@ public class asMany {
         return sell;
     }
 }
+class Solution122 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1)
+            return 0;
+        int len = prices.length;
+        int[] buy = new int[len];
+        int[] sell = new int[len];
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        for(int i = 1; i < len; i++){
+            buy[i] = Math.max(buy[i -1], sell[i-1] - prices[i]);
+            sell[i] = Math.max(sell[i - 1], buy[i-1] + prices[i]);
+        }
+        return sell[len - 1];
+    }
+}
+/*
+9 1 6 3 3 7 8 9 2
+        i
+
+
+buy[] -> [-9 -1 -1  2  2  2           ]
+sell[] -> [0  0  5  5  5  9   ]
+
+
+1. buy ->  max(sell[i-1] - price[i], buy[i -1])
+2. sell -> max(buy[i-1] + price[i], sell[i-1])
+3. do nothing
+
+return sell[n-1];
+
+
+
+
+
+
+*/
--- src/DP/Stock/coolDown.java
+++ src/DP/Stock/coolDown.java
@@ -17,3 +17,20 @@ public class coolDown {
         return sell;
     }
 }
+class Solution309 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1)
+            return 0;
+        int len = prices.length;
+        int[] buy = new int[len];
+        int[] sell = new int[len];
+        buy[0] = -prices[0];
+        buy[1] = Math.max(-prices[1], -prices[0]);
+        sell[1] = Math.max(sell[0], buy[0] + prices[1]);
+        for(int i = 2; i < len; i++){
+            buy[i] = Math.max(buy[i -1], sell[i-2] - prices[i]);
+            sell[i] = Math.max(sell[i - 1], buy[i-1] + prices[i]);
+        }
+        return sell[len - 1];
+    }
+}
