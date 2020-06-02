115111bcb431ed0673c363491711aa385ed53f24
Peng <hao.pe@husky.neu.edu>
2020-06-01 20:59:59 -0700

    LC317 updated

--- src/BFS/LC317shortestFofAllBuildings.java
+++ src/BFS/LC317shortestFofAllBuildings.java
@@ -2,6 +2,97 @@ package BFS;
 
 import java.util.LinkedList;
 import java.util.Queue;
+/*
+0 empty
+1 buildeing
+2 obstacle
+
+
+1 0 2 0 1
+0 0 0 0 0
+0 0 1 0 0
+
+
+
+0 1 1 0 0 0
+0 0 0 0 2 2
+1 0 0 1 2 0
+
+2 1 2 1 2 2 1
+
+1 b 2 b 0 0 b
+
+
+1 b b 2 3 3
+2 1 2 3 x x
+b 2 3 b x x
+
+
+there are k buildings
+
+start from each buildings to mark the distance
+
+
+2 2 1
+1 0 2
+0 1 2
+
+*/
+class Solution317 {
+    public int shortestDistance(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0)
+            return -1;
+        int row = grid.length, col = grid[0].length;
+        int[][] count = new int[row][col];
+        int[][] directions = new int[][]{{1,0},{-1,0},{0,-1},{0,1}};
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1){
+                    //calculate distance from each 1
+                    int dis = 1;
+                    boolean[][] visited = new boolean[row][col];
+                    queue.offer(i * col + j);
+                    visited[i][j] = true;
+                    while(!queue.isEmpty()){
+                        int size = queue.size();
+                        while(size-- > 0){
+                            int index = queue.poll();
+                            int ii = index / col;
+                            int jj = index % col;
+                            for(int[] direction : directions){
+                                int iii = ii + direction[0];
+                                int jjj = jj + direction[1];
+                                if(iii >= 0 && iii < row && jjj < col && jjj >= 0 && grid[iii][jjj] == 0 && !visited[iii][jjj]){
+                                    queue.offer(iii * col + jjj);
+                                    count[iii][jjj] += dis;
+                                    visited[iii][jjj] = true;
+                                }
+                            }
+                        }
+                        dis++;
+                    }
+                    for(int k = 0; k < row; k++){
+                        for(int l = 0; l < col; l++){
+                            if(grid[k][l] == 0 && !visited[k][l])
+                                grid[k][l] = 2;
+                        }
+                    }
+                }
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        //find smallest
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 0){
+                    res = Math.min(res, count[i][j]);
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+}
 
 public class LC317shortestFofAllBuildings {
     private static final int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
