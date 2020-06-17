4962910685b62202d35400404d435db1756cfc78
Peng <hao.pe@husky.neu.edu>
2020-06-17 15:54:16 -0700

    Add deDuplicateIterator.java

--- src/Iterator/deDuplicateIterator.java
+++ src/Iterator/deDuplicateIterator.java
@@ -1,4 +1,50 @@
 package Iterator;
 
-public class deDuplicateIterator {
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class deDuplicateIterator implements Iterator<Integer> {
+    private Integer next;
+    private ListIterator<Integer> iterator;
+    boolean isStart = true;
+    public deDuplicateIterator(List<Integer> list){
+        next = null;
+        iterator = list.listIterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext() || next != null;
+    }
+
+    @Override
+    public Integer next() {
+        Integer cur = null;
+        if(next == null && isStart){
+            cur = iterator.next();
+            isStart = false;
+        }
+        else{
+            cur = next;
+            next = null;
+        }
+        while (iterator.hasNext()){
+            Integer val = iterator.next();
+            if(val != cur){
+                next = val;
+                break;
+            }
+        }
+        return cur;
+    }
 }
+/*
+* 1 2 2 2 3 4 4
+*
+*
+*
+*
+*
+*
+* */
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,30 +1,14 @@
 package com.main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        temp test = new temp();
-        int[] arr = new int[]{-1,0,0,1,0,12,2,1,-2,-3,0,2,0,-7,-9,11,0,-2};
-        test.dancingArray(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-}
-class temp{
-    public void dancingArray(int[] A){
-        for(int i = -1, j = 0; j < A.length; j++){
-            if(A[j] < 0){
-                swap(A, i+1, j);
-                i++;
-            }
-        }
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        List<Integer> list = Arrays.asList(1,2,null,2,2,null,3,4,5,5);
+        deDuplicateIterator it = new deDuplicateIterator(list);
+        while(it.hasNext())
+            System.out.println(it.next());
+
     }
 }
