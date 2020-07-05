18f8d8bbf49a174ff85ab29c8a06a31f7a558687
Peng <hao.pe@husky.neu.edu>
2020-07-04 20:07:29 -0700

    Add LC315TreeWithSizeEntry

---
+++ src/Tree/LC315TreeWithSizeEntry.java
@@ -0,0 +1,47 @@
+package Tree;
+
+public class LC315TreeWithSizeEntry {
+    public List<Integer> countSmaller(int[] nums) {
+        int len = nums.length;
+        List<Integer> counts = new ArrayList<>();
+        if(nums == null || nums.length == 0) return counts;
+        TreeNode root = new TreeNode(nums[len-1]);
+        counts.add(0);
+        for(int i = len - 2; i >= 0; i--){
+            TreeNode cur = root;
+            TreeNode prev = null;
+            int count = 0;
+            while(cur != null){
+                //go left
+                if(nums[i] <= cur.val){
+                    cur.size++;
+                    prev = cur;
+                    cur = cur.left;
+                    if(cur == null)
+                        prev.left = new TreeNode(nums[i]);
+                }else{
+                    count += cur.size;
+                    prev = cur;
+                    cur = cur.right;
+                    if(cur == null)
+                        prev.right = new TreeNode(nums[i]);
+                }
+            }
+            counts.add(count);
+        }
+        Collections.reverse(counts);
+        return counts;
+    }
+
+    class TreeNode{
+        private int val;
+        private int size;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int val){
+            this.val = val;
+            size = 1;
+        }
+    }
+}
