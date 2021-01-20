4e3627d8749f1f655717b8dd66c60041d16c1546
Peng <hao.pe@husky.neu.edu>
2021-01-20 06:59:27 -0800

    add leetcode 5 dp solution

--- src/stringAndArray/LC5.java
+++ src/stringAndArray/LC5.java
@@ -40,3 +40,30 @@ public class LC5 {
         return true;
     }
 }
+class DPSolutionLC5{
+    public String longestPalindrome(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        int max = 0;
+        int start = -1, end = -1;
+        for(int i = s.length() - 1; i >= 0; i--){
+            for(int j = i; j < s.length(); j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    if(j - i + 1 <= 3){
+                        dp[i][j] = j - i + 1;
+                    }
+                    else{
+                        //这里i+1 和j-1不会越界，因为越界的情况都在j-i+1<=3的情况里边了
+                        if(dp[i + 1][j - 1] != 0)
+                            dp[i][j] = dp[i+1][j-1] + 2;
+                    }
+                    if(max < dp[i][j]){
+                        max = dp[i][j];
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+}
